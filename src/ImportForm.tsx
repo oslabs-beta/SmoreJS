5eb55fb40600f4a32c4dd61442bf6f0f3734944b
Will Ramirez <willramirez630@gail.com>
2021-06-13 23:04:28 -0400

    iframe renders properly

--- src/ImportForm.tsx
+++ src/ImportForm.tsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { createStyles, Theme} from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import ReactDom from 'react-dom';
 
 
-export default function ImportForm() {
-const state = {
-  textvalue : 'localhost:3000',
-  loadIframe : ()=>{console.log(state.textvalue)}
+interface ImportFormState {
+  textvalue: string;
+  iframe: string;
+  style: object;
+  tag: array;
 }
 
-const style = {
-  width: '600px' ,
-  height: '600px'
+interface ImportFormProps {}
+
+
+export default class ImportForm extends React.Component<ImportFormProps, ImportFormState> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      textvalue: 'http://localhost:3000',
+      iframe: '',
+      style: {width: '600px', height: '900px'},
+      tag: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 }
- return (
-    <><div>
-     <div>
-       <h3>localhost app</h3>
-       <input type="text" value={state.textvalue} />
-       <button type="button" >Load</button>
-     </div>
-   </div>
-  <iframe src={state.textvalue} style={style}></iframe >
-  </>
-  )
-        }
+
+handleSubmit(){
+  this.setState({iframe: this.state.textvalue});
+  
+  
+}
+
+handleChange(e){
+  this.setState({textvalue: e.target.value})
+}
+render(){
+return  (
+        <>
+        <div>
+        <div>
+          <h3>localhost app</h3>
+          <input type="text" value={this.state.textvalue} onChange={this.handleChange} />
+          <button type="button" onClick={this.handleSubmit}> Load </button>
+        </div>
+        </div>
+        <iframe src={this.state.iframe} style={this.state.style}></iframe>
+        
+        </>
+    )
+}}
+
